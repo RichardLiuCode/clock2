971ad5492f4357e878dbcf5e384a4417576d073e
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-05-03 17:30:47 -0700

    update

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+document.getElementById("clockBtn").addEventListener("click", function () {
+    document.querySelectorAll(".screen").forEach(function (screen) {
+        screen.style.display = "none";
+    });
+    document.getElementsByClassName("leftBar")[0].querySelectorAll(".optionBtn").forEach(function (btn) {
+        btn.classList.remove("leftBarSelectedButton");
+    });
+    document.getElementById("ClockScreen").style.display = "block";
+    this.classList.add("leftBarSelectedButton");
+});
+
+document.getElementById("timerBtn").addEventListener("click", function () {
+    document.querySelectorAll(".screen").forEach(function (screen) {
+        screen.style.display = "none";
+    });
+    document.getElementsByClassName("leftBar")[0].querySelectorAll(".optionBtn").forEach(function (btn) {
+        btn.classList.remove("leftBarSelectedButton");
+    });
+    document.getElementById("TimerScreen").style.display = "block";
+    this.classList.add("leftBarSelectedButton");
+});
+
+document.getElementById("stopwatchBtn").addEventListener("click", function () {
+    document.querySelectorAll(".screen").forEach(function (screen) {
+        screen.style.display = "none";
+    });
+    document.getElementsByClassName("leftBar")[0].querySelectorAll(".optionBtn").forEach(function (btn) {
+        btn.classList.remove("leftBarSelectedButton");
+    });
+    document.getElementById("StopwatchScreen").style.display = "block";
+    this.classList.add("leftBarSelectedButton");
+});
+
+// Clock
+
+setInterval(function () {
+    const date = new Date();
+    const Hour = (date.getHours() % 12).toString().padStart(2, "0");
+    const Minute = date.getMinutes().toString().padStart(2, "0");
+    const Time = `${Hour}:${Minute}`;
+    document.getElementById("clockTime").innerText = Time;
+}, 500);
